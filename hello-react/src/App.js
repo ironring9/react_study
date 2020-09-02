1676d2dea6a320daa492c38da2544e1370a56bd5
xpilp <leechwon92@gmail.com>
2020-09-02 23:27:50 +0900

    react study

---
+++ hello-react/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import IterationSample from './IterationSample';
+// import ValidationSample from './ValidationSample';
+// import EventPractice from './EventPractice';
+
+class App extends Component {
+  render () {
+    return (
+      <IterationSample/>
+    )
+  }
+}
+
+export default App;
