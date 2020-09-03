28582e617eafbcfd2ec6d944f471e02bda78a36e
xpilp <leechwon92@gmail.com>
2020-09-04 00:11:07 +0900

    style study

---
+++ styling-react/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SasssComponent from './SassComponent'
+
+function App() {
+  return (
+    <div>
+      <SasssComponent />
+    </div>
+  )
+}
+
+export default App;
